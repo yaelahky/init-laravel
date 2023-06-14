fc3062613a8e53a818a3a67b87a7f78a81122b71
yaelahky <kiky.lenovo@gmail.com>
2023-06-14 19:29:39 +0700

    init laravel & create rental page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Ananda Private Mengemudi</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+        <a class="navbar-brand" href="#">
+            <img src="https://www.shutterstock.com/image-vector/rent-car-logo-design-vector-260nw-1072548182.jpg"
+                alt="Logo" height="50">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#jadwal-belajar">Jadwal Belajar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#paket-jasa">Paket Jasa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#trainer-dan-pengurus">Trainer dan Pengurus</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#kendaraan">Kendaraan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#lokasi-dan-kontak">Lokasi dan Kontak</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="https://honda.arista-group.co.id/storage/file/vehicle/wGD7pvxb02gTmuFIpAVyCTisc4nixbopcwpg2aac.png"
+                    alt="Car Image" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h1 class="font-weight-bold">Masa Kini Belajar Mobil Listrik Ioniq 5</h1>
+                <p>Belajar mengemudi mobil listrik Ioniq 5 untuk kebutuhan perjalanan Anda atau kapan pun dan di mana
+                    pun Anda inginkan! Nikmati pengalaman mengemudi yang modern, ramah lingkungan, dan revolusioner
+                    dengan teknologi canggih dari Ioniq 5.</p>
+            </div>
+        </div>
+        <div class="bg-light bg-gradient">
+            <div>
+                <h3 id="jadwal-belajar">Jadwal Belajar</h3>
+            </div>
+            <section class="row w-100 mx-auto">
+                <div>
+                    <h4>Hari Selasa:</h4>
+                    <ul>
+                        <li>Jam 10:00 - 12:00 -> Ananda</li>
+                        <li>Jam 10:00 - 12:00 -> Rizky</li>
+                        <li>Jam 10:00 - 12:00 -> Yuliansyah</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Hari Rabu:</h4>
+                    <ul>
+                        <li>Jam 10:00 - 12:00 -> Ananda</li>
+                        <li>Jam 10:00 - 12:00 -> Rizky</li>
+                        <li>Jam 10:00 - 12:00 -> Yuliansyah</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Hari Kamis:</h4>
+                    <ul>
+                        <li>Jam 10:00 - 12:00 -> Ananda</li>
+                        <li>Jam 10:00 - 12:00 -> Rizky</li>
+                        <li>Jam 10:00 - 12:00 -> Yuliansyah</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Hari Minggu:</h4>
+                    <ul>
+                        <li>Jam 10:00 - 12:00 -> Ananda</li>
+                        <li>Jam 10:00 - 12:00 -> Rizky</li>
+                        <li>Jam 10:00 - 12:00 -> Yuliansyah</li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <div class="mt-5">
+            <h3 id="paket-jasa">Paket Jasa</h3>
+            <div>
+                <h4>Basic</h4>
+                <p>Belajar mengemudi dengan instruktur profesional. Durasi: 10 jam</p>
+            </div>
+            <div>
+                <h4>Regular</h4>
+                <p>Belajar mengemudi dengan instruktur profesional. Durasi: 20 jam</p>
+            </div>
+            <div>
+                <h4>Premium</h4>
+                <p>Belajar mengemudi dengan instruktur profesional. Durasi: 30 jam</p>
+            </div>
+        </div>
+        <div id="trainer-dan-pengurus" class="right mt-5">
+            <h3 class="text-right">Trainer dan Pengurus</h3>
+            <div>
+                <h4 class="text-right">Ananda</h4>
+                <p class="text-right">Pengalaman: 5 tahun di bidang menyetir tambang</p>
+            </div>
+            <div>
+                <h4 class="text-right">Rizky</h4>
+                <p class="text-right">Pengalaman: 7 tahun di bidang otomotif</p>
+            </div>
+            <div>
+                <h4 class="text-right">Yuliansyah</h4>
+                <p class="text-right">Pengalaman: 3 tahun di bidang otomotif</p>
+            </div>
+        </div>
+
+        <div id="kendaraan" class="mt-5">
+            <h1 class="text-center">Rasakan Ioniq 5</h1>
+            <p class="text-center">Temukan masa depan mobilitas listrik dengan Ioniq 5 kami yang bergaya dan ramah
+                lingkungan. Dilengkapi dengan teknologi canggih dan powertrain yang berkelanjutan, Ioniq 5 memberikan
+                pengalaman berkendara yang mulus.</p>
+            <div class="text-center">
+                <img src="https://honda.arista-group.co.id/storage/file/vehicle/wGD7pvxb02gTmuFIpAVyCTisc4nixbopcwpg2aac.png"
+                    alt="Car Image" style="max-height: 200px; max-width: fit-content;">
+            </div>
+        </div>
+
+        <div id="lokasi-dan-kontak" class="mt-5 text-center mb-5">
+            <h1 class="text-center">Hubungi kami</h1>
+            <div class="text-center">Jalan Raya Binus Utara, Jakarta Selatan</div>
+            <div class="text-center mb-3">WhatsApp: 082231509025 atau</div>
+            <button class="btn btn-success">Klik WhatsApp Disini</button>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
